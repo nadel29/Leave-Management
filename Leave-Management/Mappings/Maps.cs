35a9ed3460f101bf7b42aa37e5b8c4732240a27d
ndftba <nido.adel@gmail.com>
2020-07-28 11:11:28 +0200

    Add project files.

---
+++ Leave-Management/Mappings/Maps.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Leave_Management.Data;
+using Leave_Management.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_Management.Mappings
+{
+    public class Maps : Profile
+    {
+        public Maps()
+        {
+            //They can be mapped in both directions
+            CreateMap<LeaveType, DetailsLeaveTypeVM>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeVM>().ReverseMap();
+
+            CreateMap<Employee, EmployeeVM>().ReverseMap();
+            CreateMap<LeaveHistory, LeaveHistoryVM>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationVM>().ReverseMap();
+        }
+    }
+}
---
+++ Leave-Management/Models/LeaveAllocationVM.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_Management.Models
+{
+    public class LeaveAllocationVM
+    {
+        public int Id { get; set; }
+        public int NumberOfDays { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public EmployeeVM Employee { get; set; }
+        public string EmployeeId { get; set; }
+
+        public DetailsLeaveTypeVM LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+
+        //For Dropdownlist of Employees and LeaveTypes
+        public IEnumerable<SelectListItem> Employees { get; set; }
+        public IEnumerable<SelectListItem> LeaveTypes { get; set; }
+    }
+}
